d61078603d7bc49aa2dafc5056fec118b8d2a636
Didier Guerrero <146674226+Didier-Guerrero@users.noreply.github.com>
2025-06-20 10:28:27 +0000

    Preparacion v1.1.0

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main{
     public static void main(String[] args){
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
-        System.out.println(saludo.generarSaludo("Version 1.0.0 lista para produccion"));
+        System.out.println(saludo.generarSaludo("Version 1.1.0 lista para produccion"));
         Despedida despedida = new Despedida();
         System.out.println(despedida.generarDespedida("Mundo"));
     }
